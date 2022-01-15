eaf939dc90a30c43263ed1d9c1f6b8275011b3a6
SavinovDanil <savinov.danil18@outlook.com>
2022-01-15 15:17:24 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import 'macro-css';
 
 import Card from './components/Card'
@@ -53,20 +54,32 @@ function App() {
   
   const [cartOpened, setCartOpened] = React.useState(false);
   const [cartItems, setCartItems] = React.useState([]) 
-
-  const onAddtoCarts = (obj) => {
-    setCartItems(prev => [...prev, obj])
-  }
+  const [searchValue, setSearchValue] = React.useState('')
 
   React.useEffect(() => {
-    fetch('https://61e182c263f8fc0017618cb3.mockapi.io/items')
-    .then((res) => {
-      return res.json()
-    .then((obj) => {
-        setItems(obj)
-      })
+    axios.get('https://61e182c263f8fc0017618cb3.mockapi.io/items').then(res => {
+      setItems(res.data)
+    })
+
+    axios.get('https://61e182c263f8fc0017618cb3.mockapi.io/cart').then(res => {
+      setCartItems(res.data)
     })
   }, [])
+
+
+  const onAddtoCarts = (obj) => {
+    axios.post('https://61e182c263f8fc0017618cb3.mockapi.io/cart', obj)
+    setCartItems(prev => [...prev, obj])
+
+  }
+
+  const onChangeValueInput =(event) => {
+    setSearchValue(event.target.value)
+  }
+
+
+
+
 
   return (
     <div className="wrapper clear">
@@ -74,15 +87,19 @@ function App() {
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
-          <h1 className="contentHeading">Все кроссовки</h1>
+          
+          <h1 className="contentHeading">{searchValue ? `Поиск по фильтру: ${searchValue}` : 'Все кроссовки'}</h1>
           <div className='search-block d-flex align-center'>
             <img className='ml-10' src="/img/search.svg" alt="search" width={14} height={14}/>
-            <input  placeholder='Поиск...'/>
+            {searchValue && (<img src="/img/btn-remove.svg" onClick={() => setSearchValue('')} alt="" className='clear cu-p'/>)}
+            <input onChange={onChangeValueInput} value={searchValue} placeholder='Поиск...'/>
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {items.map(item => (
+          {items.filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((item, index) => (
             <Card 
+            key={index}
             title={item.title}
             imageUrl={item.imageUrl}
             price={item.price}
